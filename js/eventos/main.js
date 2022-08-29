b5390cb1bc0919c4ca3d4275d593a3a5f11e4d6a
uelionobre <uelionobre@hotmail.com>
2022-08-29 18:49:20 -0300

    resolve conteúdo parte 2

--- js/eventos/main.js
+++ js/eventos/main.js
@@ -8,16 +8,42 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+/**
+ * 
+ * Esse efeito ocorre por conta da propriedade TranslateY do CSS.
+ */
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+function addClassToTagLi(event) {
+  removeClassTechToLi();
+  event.target.className = 'tech';
+}
+function removeClassTechToLi() {
+  let lis = document.getElementsByTagName('li');
+  for (li of lis) li.removeAttribute('class');
+}
+
+firstLi.addEventListener('click', addClassToTagLi)
+secondLi.addEventListener('click', addClassToTagLi)
+thirdLi.addEventListener('click', addClassToTagLi)
+
+
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+function addTextToClassTech(event) {
+  document.querySelector('.tech').innerText = event.target.value;
+}
+input.addEventListener('keyup', addTextToClassTech)
+input.addEventListener('focusout', function(e){e.target.value = ''})
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+myWebpage.addEventListener('dblclick', function () {
+  window.open('https://uelionobre.github.io', '_blank')
+})
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
@@ -33,6 +59,8 @@ function resetText(event) {
 }
 
 firstLi.addEventListener('dblclick', resetText);
+myWebpage.addEventListener('mouseover', function (e) { e.target.style.color = '#2fc18c' });
+myWebpage.addEventListener('mouseleave', function(e){e.target.style.color = 'white'})
 
 // Não precisa passar o parâmetro dentro da callback resetText. O próprio
 // navegador fará esse trabalho por você, não é legal? Desse jeito, o
